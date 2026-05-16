273bfe213523c4014b035460dc55faf42fd93691
GynUnti <marknguyen0905@gmail.com>
2026-05-16 18:40:51 -0400

    05/16/2026 current dotfiles

---
+++ i3/set_once.sh
@@ -0,0 +1,8 @@
+# Set wallpaper
+feh --bg-scale /usr/share/endeavouros/backgrounds/Evangelion\ Unit\ Wallpaper.jpg
+
+# Remap CapsLock to ESC
+setxkbmap -option caps:escape
+
+# Activate fcitx5 for multiple languages keyboard
+fcitx5 -d
